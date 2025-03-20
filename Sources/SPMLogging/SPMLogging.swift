29a66cf0bd1efc4bb4395789ac862578f52a2e87
Jithin Balan <jithinpala@gmail.com>
2025-03-20 16:06:54 +1100

    Initial commit

---
+++ Sources/SPMLogging/SPMLogging.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct SPMLogging {
+    private let name = "SPMLoggingName"
+    
+    func showName() {
+        print("===================")
+        print("⚙️ Name is: \(name)")
+        print("===================")
+    }
+}
---
+++ Sources/SPMNetwork/SPMMyLibrary.swift
@@ -0,0 +1,12 @@
+
+import Foundation
+
+struct SPMNetwork {
+    private let name = "SPMNetworkName"
+    
+    func showName() {
+        print("===================")
+        print("⚙️ Name is: \(name)")
+        print("===================")
+    }
+}
